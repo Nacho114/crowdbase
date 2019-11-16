57af5a1adc467755f45429043b170f0d52a8f84f
Nacho114 <sukarno.bastian@outlook.com>
2019-11-16 10:34:14 +0100

    cleaned old server stuff

--- server/db_test.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-
-connection = psycopg2.connect(user = "nanarry",
-                                password = "nanaRRYhack20",
-                                host = "db-crowdbase.cbkzmw7o5c46.eu-central-1.rds.amazonaws.com",
-                                port = "5432",
-                                database = "postgres")
-
-cursor = connection.cursor()
