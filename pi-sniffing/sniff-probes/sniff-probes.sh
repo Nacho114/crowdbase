ffa1774928cca2dc5805a3093a3dd2c9f5a2f032
Thierry <thierry.bossy@romandie.com>
2019-11-16 18:03:15 +0100

    update

--- pi-sniffing/sniff-probes/sniff-probes.sh
+++ pi-sniffing/sniff-probes/sniff-probes.sh
@@ -40,7 +40,7 @@ function send {
     while read LINE; do
         DEV_ID=${DEVICEID:="UNKNOWN"}
         TIMESTAMP=$(date +%s)
-        SIGNAL=$(echo $LINE | cut -f2 -d ' ')
+        SIGNAL=$(echo $LINE | cut -f3 -d ' ')
         MAC=$(echo $LINE | cut -f3 -d ' ')
         SSID=$(echo $LINE | cut -f4 -d ' ')
 
